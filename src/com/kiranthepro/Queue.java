e963113e70f617972b9393fc7ff1d3dc30358745
Chaitran77 <kiran@inteleyes.com>
2022-10-12 16:06:06 +0100

    Initial commit

---
+++ src/com/kiranthepro/Queue.java
@@ -0,0 +1,29 @@
+package com.kiranthepro;
+
+public class Queue {
+
+    private int[] data;
+
+    public Queue(int size) {
+        data = new int[size];
+    }
+
+    private int rearIndex() {
+        boolean reachedEnd = false;
+
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == null) {
+                reachedEnd = true;
+                break;
+            }
+        }
+
+
+    }
+
+    public void enQueue(int value) {
+        data[]
+    }
+
+
+}
